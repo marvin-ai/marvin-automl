985f4d5d3ca1666ca051a236d31ad912ef42c25f
Willian Leite <willian.leite@gmail.com>
2018-06-09 12:22:29 -0300

    Adding eslint, jest and fix deprecated express sendfile

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var express  = require('express');
-var app = express();
+const express  = require('express');
+const app = express();
 
-var bodyParser = require('body-parser');    
+const bodyParser = require('body-parser');    
 
 app.use(express.static(__dirname + '/public')); 
 
 // application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // application/json
-app.use(bodyParser.json());                                     
-
-app.listen(8080);
+app.use(bodyParser.json());
 
 // routes
 // application 
 app.get('*', function(req, res) {
-    res.sendfile('./public/index.html'); 
+    res.sendFile(__dirname + '/public/index.html'); 
 });
 
-console.log("Marvin AutoML listening on port 8080");
+app.listen(8080, () => {
+    console.log("Marvin AutoML listening on port 8080");
+});
