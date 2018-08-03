ed162dae2158a7535bc5c02c3bd8d72ebb2562b2
Rafael Novello <rafa.reis.novello@gmail.com>
2018-08-03 13:56:01 -0300

    Little adjustments

--- src/app/acquisitor/acquisitor.component.ts
+++ src/app/acquisitor/acquisitor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -10,18 +11,19 @@ import { UserService } from '../user.service';
 
 
 export class AcquisitorComponent implements OnInit {
-   constructor(private user:UserService) { }
-   ngOnInit() {
+    constructor(private user:UserService, private http: HttpClient) { }
+    ngOnInit() {
   }
 
-  submitAcquisitor(post) {
-  	 post.preventDefault();
-    //console.log(post);
-    var target = post.target.elements[0].value;
-    var url = post.target.elements[1].value;
-    var sep = post.target.elements[2].value;
-    var encoding = post.target.elements[3].value;
-    var generations = post.target.elements[4].value;
-    
-  }
+    submitAcquisitor(post) {
+        post.preventDefault();
+        //console.log(post);
+        var url = post.target.elements[0].value;
+        var target = post.target.elements[1].value;
+        var sep = post.target.elements[2].value;
+        var encoding = post.target.elements[3].value;
+        var generations = post.target.elements[4].value;
+
+        // Post go here
+    }
 }
